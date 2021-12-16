e5e27f7793408d9e96695c2fc27ec20508152c8f
anselme <andesanselme@gmail.com>
2021-12-16 14:45:27 +0200

    fix: the city service to convert the whether into farnheit

--- src/main/java/rw/ac/rca/termOneExam/service/CityService.java
+++ src/main/java/rw/ac/rca/termOneExam/service/CityService.java
@@ -13,28 +13,42 @@ import rw.ac.rca.termOneExam.repository.ICityRepository;
 @Service
 public class CityService {
 
-	@Autowired
-	private ICityRepository cityRepository;
-	
-	public Optional<City> getById(long id) {
-		
-		return cityRepository.findById(id);
-	}
+    @Autowired
+    private ICityRepository cityRepository;
 
-	public List<City> getAll() {
-		
-		return cityRepository.findAll();
-	}
+    public Optional<City> getById(long id) {
 
-	public boolean existsByName(String name) {
-		
-		return cityRepository.existsByName(name);
-	}
+        Optional<City> city = cityRepository.findById(id);
 
-	public City save(CreateCityDTO dto) {
-		City city =  new City(dto.getName(), dto.getWeather());
-		return cityRepository.save(city);
-	}
-	
+        // add a line to convert to set the degrees to farneheit
+        city.ifPresent(value -> value.setFahrenheit(converter(value.getWeather())));
 
+        return city;
+    }
+
+    public List<City> getAll() {
+
+        List<City> cities = cityRepository.findAll();
+
+        // add a for loop to set the degrees to farneheit
+        for (City city : cities)
+            city.setFahrenheit(converter(city.getWeather()));
+
+        return cities;
+    }
+
+    public boolean existsByName(String name) {
+
+        return cityRepository.existsByName(name);
+    }
+
+    public City save(CreateCityDTO dto) {
+        City city = new City(dto.getName(), dto.getWeather());
+        return cityRepository.save(city);
+    }
+
+
+    public double converter(double celius) {
+        return (celius * 9 / 5) + 32;
+    }
 }
